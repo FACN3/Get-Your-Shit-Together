4427f2806a26e6e9c301f637dd146f2068b815a5
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-11-07 15:38:44 +0200

    adding the secound function with tests
    based on issue #3

--- logic.js
+++ logic.js
@@ -10,20 +10,23 @@ var todoFunctions = {
 
     function incrementCounter() {
       return (idCounter += 1);
+
     }
 
     return incrementCounter;
   })(),
-  
-  //cloneArrayOfObjects will create a copy of the todos array 
+
+  //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
     return todos.map(function(todo){
       return JSON.parse(JSON.stringify(todo));
     });
   },
-  
+
   addTodo: function(todos, newTodo) {
+    newTodo['id']=todoFunctions.generateId();
+    return todos.concat(newTodo);
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
@@ -33,6 +36,11 @@ var todoFunctions = {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
+  //  var newTodos = todoFunctions.cloneArrayOfObjects(todos);
+    return todos.filter(function(object){
+      return object["id"] != idToDelete;
+    });
+
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
@@ -51,7 +59,7 @@ var todoFunctions = {
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details: 
+// See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
   module.exports = todoFunctions;
--- tests/logic.tests.js
+++ tests/logic.tests.js
@@ -1,14 +1,40 @@
 var test = require('tape');
 var logic = require('../logic.js');
 var newTodo={
-  id:"",
-  description:"hello",
-  done: false;
+  "id":"",
+  "description":"hello",
+  "done": false
 };
-var todoList = [{id:"", description:"li1", done: false;},{id:"", description:"li2", done: false;}];
+
 test("tests for addTodo",function(t){
+
+  var actual = logic.addTodo([],newTodo);
+  var expected = [{"id":1,"description":"hello","done": false}];
+  t.deepEqual(actual,expected,"your new todo was added to an empty array");
+  t.end();
+});
+
+
+var todoList = [{"id":1, "description":"li1", "done": false},{"id":2, "description":"li2", "done": false}];
+test("tests for addTodo",function(t){
+
+ logic.generateId();
   var actual = logic.addTodo(todoList,newTodo);
-  var expected = [{id:"", description:"li1", done: false;},{id:"", description:"li2", done: false;},{id:"",description:"hello",done: false;}];
-  t.equal(actual,expected,"your new todo was not added");
+  var expected = [{"id":1, "description":"li1", "done": false},{"id":2, "description":"li2", "done": false},{"id":3,"description":"hello","done": false}];
+  t.deepEqual(actual,expected,"your new todo was added to an existing array");
+  t.end();
+});
+test("test for deleting in deleteTodo",function(t){
+  var array =[{"id":1, "description":"li1", "done": false},{"id":2, "description":"li2", "done": false},{"id":3,"description":"hello","done": false}];
+  var actual = logic.deleteTodo(array,2);
+  var expected = [{"id":1, "description":"li1", "done": false},{"id":3,"description":"hello","done": false}];
+  t.deepEqual(actual,expected,"your todo was deleted from the an existing array");
+  t.end();
+});
+test("test for deleting in deleteTodo",function(t){
+  var array =[{"id":1, "description":"li1", "done": false},{"id":2, "description":"li2", "done": false},{"id":3,"description":"hello","done": false}];
+  var actual = logic.deleteTodo(array,2);
+  var expected = [{"id":1, "description":"li1", "done": false},{"id":3,"description":"hello","done": false}];
+  t.notEqual(actual,expected,"your todo was deleted from the an existing array");
   t.end();
 });

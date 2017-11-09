37f95ef91b42002b489ec15fdf396bcc57bc5dbf
MynahMarie <mynah.marie@gmail.com>
2017-11-09 11:29:47 +0200

    Fix the Object.values[3] to replace it with object.id
    Relates #39

--- logic.js
+++ logic.js
@@ -56,7 +56,8 @@ var todoFunctions = {
   //sophisticated version
   console.log(todos);
   return todos.map(function(object){
-    object["done"] = Object.values(object)[3] == idToMark ?  !object["done"] : object["done"];
+    //object["done"] = Object.values(object)[3] == idToMark ?  !object["done"] : object["done"];
+    object["done"] = object.id == idToMark ?  !object["done"] : object["done"];
     return object;
    });
   },
